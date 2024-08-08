11cc411f267faf85cd6aad084b3de5b5e76e36ae
Mohsen <seyedmohsen.shafaei@innowise.com>
2024-08-08 16:13:04 +0200

    implementation of graphql guard and creating bussines logics for authentication

---
+++ backend/src/auth/dto/login.dto.ts
@@ -0,0 +1,15 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty} from 'class-validator';
+
+
+@InputType()
+export class LoginDto {
+  @Field()
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsEmail({}, { message: 'Email must be valid.' })
+  email: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Password is required.' })
+  password: string;
+}
---
+++ backend/src/common/constants/time.constant.ts
@@ -0,0 +1,6 @@
+const  TimeConstant = {
+    jwt_access_exp:1500,
+    jwt_refresh_exp:15000
+}
+
+export default TimeConstant
