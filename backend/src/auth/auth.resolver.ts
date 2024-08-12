ff817102d2158a7c9242aad4dfe45d8d56b32b58
Mohsen <seyedmohsen.shafaei@innowise.com>
2024-08-12 16:52:03 +0200

    Finishing the first stage of backend with Auth and User Modules

--- backend/src/auth/auth.resolver.ts
+++ backend/src/auth/auth.resolver.ts
@@ -1,10 +1,53 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LoginResponse, RegisterResponse } from './types';
+import { RegisterDto } from './dto/register.dto';
+import { Request, Response } from 'express';
+import { BadRequestException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Resolver()
 export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
 
-    @Query(()=> String)
-    async hello(){
-        return "Hello"
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDto,
+    @Context() Context: { res: Response },
+  ) {
+    if (registerDto.password !== registerDto.confirmPassword) {
+      throw new BadRequestException({
+        confirmPassword: 'Password and confirmPassword does not match',
+      });
     }
+    const { user } = await this.authService.register(registerDto, Context.res);
+    return { user };
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Args('loginInput') loginDto: LoginDto,
+    @Context() Context: { res: Response },
+  ) {
+    return this.authService.login(loginDto, Context.res);
+  }
+
+  @Mutation(() => String)
+  async logout(@Context() context: { res: Response }) {
+    return this.authService.logout(context.res);
+  }
+
+  @Query(() => String)
+  async hello() {
+    return 'Hello';
+  }
+
+  @Mutation(() => String)
+  async refreshToken(@Context() Context: { req: Request; res: Response }) {
+    try {
+      return this.authService.refreshToken(Context.req, Context.res);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
 }
--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,4 +1,40 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from './user.type';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/Guard/graphql.auth.guard';
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
+import { Request } from 'express';
+import { join } from 'path';
+import { createWriteStream } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
-@Resolver()
-export class UserResolver {}
+
+@Resolver() 
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @UseGuards(GraphqlAuthGuard)
+  @Mutation(() => User)
+  async updateUser(
+    @Args('fullname') fullname: string,
+    @Args('file', { type: () => GraphQLUpload, nullable: true })
+    file: GraphQLUpload.FileUpload,
+    @Context() context: { req: Request },
+  ) {
+    const imageUrl = file? await this.storeImageAndGetUrl(file):null;
+    const userId = context.req.user?.sub;
+    return this.userService.updateUser(userId, fullname, imageUrl)
+  }
+
+  private async storeImageAndGetUrl(file: GraphQLUpload) {
+    const { createReadStream, filename } = await file;
+    const uniqueFilename = `${uuidv4()}_${filename}`;
+    const imagePath = join(process.cwd(), 'public', 'images', uniqueFilename);
+    const imageUrl = `${process.env.APP_URL}/images/${uniqueFilename}`;
+    const readStream = createReadStream();
+    readStream.pipe(createWriteStream(imagePath));
+    return imageUrl;
+  }
+
+}
