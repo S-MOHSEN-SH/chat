b730f7510d97ef7b14cef5999a775337a01e1c50
Mohsen <seyedmohsen.shafaei@innowise.com>
2024-08-22 16:39:47 +0200

    defining client and generating graphql models using codegen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { MantineProvider } from "@mantine/core";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./apolloClient.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider>
+    <ApolloProvider client={client}>
+      
+        <App />
+      
+    </ApolloProvider>
+    </MantineProvider>
+  </React.StrictMode>
+);
